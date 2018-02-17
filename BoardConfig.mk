bf8d0f0bc45f004a1c7f9a6f05193e7e17f1a373
codeworkx <daniel.hillenbrand@codeworkx.de>
2018-02-17 21:11:27 +0100

    RMX1971: wifi: Wait for kernel driver to get ready
    Depends on : https://review.lineageos.org/206958
    
    Change-Id: Ieced27fb0bb5cba6d9850de594b5d4df7977991d
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -188,6 +188,7 @@ BOARD_HOSTAPD_PRIVATE_LIB := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
 BOARD_WPA_SUPPLICANT_DRIVER := NL80211
 BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
 WPA_SUPPLICANT_VERSION := VER_0_8_X
+WIFI_DRIVER_OPERSTATE_PATH := "/sys/class/net/wlan0/operstate"
 
 # Inherit from the proprietary version
 -include vendor/realme/RMX1971/BoardConfigVendor.mk
