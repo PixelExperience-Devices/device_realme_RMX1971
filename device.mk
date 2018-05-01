0012ef02616b75f23936f3a86aca5acd3e0079c8
Jesse Chan <jc@lineageos.org>
2018-04-30 23:02:55 -0700

    RMX1971: overlay: Enable option for full screen aspect ratio
    Change-Id: I2a1b2acc5ed97ccb6795dc371c2b3ac9cb4f9d03
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

--- device.mk
+++ device.mk
@@ -18,7 +18,8 @@ TARGET_SCREEN_WIDTH := 1080
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-lineage
 
 # Audio
 PRODUCT_PACKAGES += \
