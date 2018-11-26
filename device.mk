d94e1fa5fde5eb278152fd2849b2bb8780c6acb4
Wileen Chiu <wileenc@codeaurora.org>
2018-11-26 12:08:55 -0800

    RMX1971: Add support for telephony hidl wrapper
    Change-Id: I4e248bb6de482d503e672d284189b24cafac8d20
    Signed-off-by: BabluS <baaswanthmadhav@gmail.com>

--- device.mk
+++ device.mk
@@ -398,6 +398,8 @@ PRODUCT_SOONG_NAMESPACES += $(LOCAL_PATH)
 
 # Telephony
 PRODUCT_PACKAGES += \
+    qti-telephony-hidl-wrapper \
+    qti_telephony_hidl_wrapper.xml \
     telephony-ext
 
 #PRODUCT_BOOT_JARS += \
