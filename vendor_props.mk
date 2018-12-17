64f2f6c32dc061637df6090de4337fb9e1906506
Sundong Ahn <sundongahn@google.com>
2018-12-17 16:40:39 +0900

    RMX1971: Add system properties for surfaceflinger
    The configstore will be not used in surfaceflinger. So surfaceflinger
    will use the system properties instead of configstore.
    For this, the system properties are added.
    
    Bug: 112386364
    Test: m -j && boot test
    Change-Id: Ie3d2adccca06702217984cf2c9db986d852b7014
    Signed-off-by: bablusss <baaswanthmadhav@gmail.com>

--- vendor_props.mk
+++ vendor_props.mk
@@ -126,3 +126,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.radio.custom_ecc=1 \
     persist.vendor.radio.rat_on=combine \
     persist.vendor.radio.sib16_support=1
+
+# SurfaceFlinger
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    ro.surface_flinger.has_wide_color_display=true \
+    ro.surface_flinger.has_HDR_display=true \
+    ro.surface_flinger.use_color_management=true \
+    ro.surface_flinger.wcg_composition_dataspace=143261696
