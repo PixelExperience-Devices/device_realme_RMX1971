d839f74e537144f0273adaef8abbf894c6c6bca5
Paul McLean <pmclean@google.com>
2018-12-17 08:14:18 -0700

    RMX1971: Adding Audio HAL V5: Direction API
    Bug: 119137468
    Test: With testbed app
    Change-Id: I4251b3853d00308dbd988625f8e7466a66b823fa
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    Signed-off-by: BabluS <baaswanthmadhav@gmail.com>

--- device.mk
+++ device.mk
@@ -85,13 +85,11 @@ PRODUCT_PACKAGES += \
 
 # Audio
 PRODUCT_PACKAGES += \
-    android.hardware.audio@4.0-impl \
-    android.hardware.audio@2.0-impl \
+    android.hardware.audio@5.0-impl \
     android.hardware.audio@2.0-service \
-    android.hardware.audio.effect@4.0-impl \
-    android.hardware.audio.effect@2.0-impl \
     android.hardware.audio.effect@2.0-service \
     android.hardware.soundtrigger@2.2-impl \
+    android.hardware.audio.effect@5.0-impl \
     audio.primary.sdm710 \
     audio.a2dp.default \
     audio.r_submix.default \
