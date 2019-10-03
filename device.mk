6a81ee16c32c35a8169d4c45c0441a259b32e530
SagarMakhar <sagarmakhar@gmail.com>
2019-10-03 15:30:26 +0000

    RMX1971: Add stock system props
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    Signed-off-by: BabluS <baaswanthmadhav@gmail.com>

--- device.mk
+++ device.mk
@@ -21,6 +21,9 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-lineage
 
+# Properties
+-include $(LOCAL_PATH)/system_prop.mk
+
 # AID/fs configs
 PRODUCT_PACKAGES += \
     fs_config_files
