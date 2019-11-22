0db38ca403d6f85a6c8421aa6f5211b89dd2cbdc
BabluS <baaswanthmadhav@gmail.com>
2019-11-22 11:08:03 +0530

    RMX1971: Import msm_irqbalance.conf from stock
    Signed-off-by: BabluS <baaswanthmadhav@gmail.com>

--- device.mk
+++ device.mk
@@ -211,6 +211,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     ims-ext-common
 
+# IRQ
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/msm_irqbalance.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance.conf
+
 # Lights
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.realme_sdm710 \
