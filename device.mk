6ddcb1854fe7f0bb4d63929930ba0ce6d3e52326
BabluS <baaswanthmadhav@gmail.com>
2019-11-22 11:19:02 +0530

    RMX1971: Build Ant+ varient, camera & CNE packages
    Signed-off-by: BabluS <baaswanthmadhav@gmail.com>

--- device.mk
+++ device.mk
@@ -76,6 +76,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     fs_config_files
 
+# ANT+
+PRODUCT_PACKAGES += \
+    AntHalService
+
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio@4.0-impl \
@@ -136,7 +140,17 @@ PRODUCT_COPY_FILES += \
 
 # Camera
 PRODUCT_PACKAGES += \
+    android.frameworks.displayservice@1.0 \
+    android.hardware.camera.provider@2.4-impl \
+    android.hardware.camera.provider@2.4-service \
+    libxml2 \
     Snap
+
+# CNE
+PRODUCT_PACKAGES += \
+    cneapiclient \
+    com.quicinc.cne \
+    services-ext
 
 # Device init scripts
 PRODUCT_PACKAGES += \
