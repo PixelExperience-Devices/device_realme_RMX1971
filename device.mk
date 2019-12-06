5004f7bdaf3b7880bd08848aaedaa2f3f037283b
bablusss <baaswanthmadhav@gmail.com>
2019-12-06 10:59:57 +0000

    RMX1971: power hal 1.0
    Signed-off-by: bablusss <baaswanthmadhav@gmail.com>

--- device.mk
+++ device.mk
@@ -329,7 +329,8 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.1-service-qti
+    android.hardware.power@1.0-service \
+    android.hardware.power@1.0-impl
 
 # QMI
 PRODUCT_PACKAGES += \
