d6e4023f9bd1d565a6cc80091605aa56c7ee53eb
SamarV-121 <samarvispute121@gmail.com>
2020-03-26 00:13:58 +0530

    RMX1971: Enable TARGET_USES_INTERACTION_BOOST
    Signed-off-by: SamarV-121 <samarvispute121@gmail.com>
    Signed-off-by: bablusss <baaswanthmadhav@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -158,6 +158,7 @@ BOARD_USES_QCOM_HARDWARE := true
 
 # Power
 TARGET_TAP_TO_WAKE_NODE := "/proc/touchpanel/double_tap_enable"
+TARGET_USES_INTERACTION_BOOST := true
 
 # Recovery
 TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/recovery.fstab
