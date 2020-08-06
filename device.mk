4dd211f4230923c85bf5362aeae623604ac49b07
bablusss <baaswanthmadhav@gmail.com>
2020-08-06 09:09:17 +0530

    RMX1971: Update camera blobs to Q

--- device.mk
+++ device.mk
@@ -151,10 +151,10 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    android.frameworks.displayservice@1.0 \
+    android.hardware.camera.provider@2.4-service_64  \
     android.hardware.camera.provider@2.4-impl \
-    android.hardware.camera.provider@2.4-service \
-    libxml2 \
+    vendor.qti.hardware.camera.device@1.0.vendor \
+    libstdc++.vendor \
     Snap
 
 # CNE
