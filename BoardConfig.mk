782edeacb464801f848efbb6512247a7637b6d54
MadhavSaladi <baaswanthmadhav@gmail.com>
2020-09-03 16:25:23 +0530

    Revert "RMX1901: Add sepolicies for Motor and FOD hal"
    This reverts commit 9bec2037c94923c86bbb187920574b7c3b4e8d2a.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -20,9 +20,6 @@ TARGET_KERNEL_CONFIG := RMX1971_defconfig
 
 BOARD_BUILD_SYSTEM_ROOT_IMAGE := true
 
-# Sepolicy
-BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
-
 # Verified Boot
 BOARD_AVB_ENABLE := true
 BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flag 2
