3b82d5b53f136a56b906d3e58c24c481233f5891
MadhavSaladi <baaswanthmadhav@gmail.com>
2020-09-03 16:38:37 +0530

    MX1971: Update audio configs
    Signed-off-by: MadhavSaladi <baaswanthmadhav@gmail.com>

--- device.mk
+++ device.mk
@@ -32,8 +32,8 @@ $(call inherit-product, device/realme/sdm710-common/sdm710.mk)
 
 # Audio
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/audio/mixer_paths_18041.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_18041.xml \
-    $(LOCAL_PATH)/audio/audio_platform_info_18041.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info_18041.xml \
+    $(LOCAL_PATH)/audio/mixer_paths_19691.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_19691.xml \
+    $(LOCAL_PATH)/audio/audio_platform_info_19691.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info_19691.xml \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
