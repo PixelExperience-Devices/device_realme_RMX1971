7f21b3826fc20733e1bb85cb60dd07209d3ce616
MadhavSaladi <baaswanthmadhav@gmail.com>
2020-12-26 12:45:06 +0530

    RMX1971: Init PE
    Signed-off-by: MadhavSaladi <baaswanthmadhav@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,4 +5,4 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_RMX1971.mk
+    $(LOCAL_DIR)/aosp_RMX1971.mk
--- aosp_RMX1971.mk
+++ aosp_RMX1971.mk
@@ -4,15 +4,18 @@
 # SPDX-License-Identifer: Apache-2.0
 #
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common PixelExperience stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Inherit from RMX1971 device.
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_GAPPS_ARCH := arm64
+
 # Device identifier. This must come after all inclusions.
 PRODUCT_DEVICE := RMX1971
-PRODUCT_NAME := lineage_RMX1971
+PRODUCT_NAME := aosp_RMX1971
 PRODUCT_BRAND := Realme
 PRODUCT_MODEL := Realme 5 Pro
 PRODUCT_MANUFACTURER := Realme
